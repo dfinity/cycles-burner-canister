ed72df0cab749e51e8ee8316784ba97fe324e3ec
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-11 08:36:53 +0000

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,21 @@
-mod config;
 mod storage;
 
-use crate::config::Config;
 use ic_cdk_macros::{init, query};
 use std::cell::RefCell;
 
 thread_local! {
     /// The local storage for the configuration.
-    static CONFIG: RefCell<Config> = RefCell::new(Config::new());
+    static CONFIG: RefCell<u128> = RefCell::new(0);
 }
 
 /// This function is called when the canister is created.
 #[init]
-fn init(config: Config) {
+fn init(config: u128) {
     crate::storage::set_config(config);
 }
 
 /// Returns the config.
 #[query]
-fn get_config() -> Config {
+fn get_config() -> u128 {
     crate::storage::get_config()
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,12 +1,11 @@
-use crate::config::Config;
 use crate::CONFIG;
 
 /// Returns the configuration from the local storage.
-pub fn get_config() -> Config {
+pub fn get_config() -> u128 {
     CONFIG.with(|cell| cell.borrow().clone())
 }
 
 /// Sets the configuration in the local storage.
-pub fn set_config(config: Config) {
+pub fn set_config(config: u128) {
     CONFIG.with(|cell| *cell.borrow_mut() = config);
 }
