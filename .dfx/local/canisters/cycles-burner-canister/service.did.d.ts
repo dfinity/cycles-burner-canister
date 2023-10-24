4b20f60ae8313a222764fcf76206a0f16342fc4f
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-24 12:35:34 +0000

    .

--- .dfx/local/canisters/cycles-burner-canister/service.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface config {
-  'burn_rate' : bigint,
-  'interval_between_timers_in_seconds' : bigint,
-}
-export interface _SERVICE { 'get_config' : ActorMethod<[], config> }
--- .dfx/local/canisters/cycles-burner-canister/service.did.js
+++
@@ -1,8 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const config = IDL.Record({
-    'burn_rate' : IDL.Nat,
-    'interval_between_timers_in_seconds' : IDL.Nat64,
-  });
-  return IDL.Service({ 'get_config' : IDL.Func([], [config], ['query']) });
-};
-export const init = ({ IDL }) => { return []; };
