ca7e12cf326e91eb5f30d81e5d09c7517fde0296
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-24 12:34:27 +0000

    .

--- e2e-tests/scenario.sh
+++ e2e-tests/scenario.sh
@@ -15,38 +15,38 @@ trap "dfx stop" EXIT SIGINT
 dfx start --background --clean
 
 INITIAL_BALANCE=100000000000
-BURN_RATE=10000000000
+BURN_RATE="10_000_000_000"
 INTERVAL=5
 
 dfx deploy --no-wallet --with-cycles "$INITIAL_BALANCE" cycles-burner-canister --argument "(record {
-    burn_rate = "$BURN_RATE";
-    interval_between_timers_in_seconds = "$INTERVAL";
+    burn_rate = $BURN_RATE;
+    interval_between_timers_in_seconds = $INTERVAL;
 })"
 
 CONFIG=$(dfx canister call --query cycles-burner-canister get_config)
 EXPECTED_CONFIG="(
   record {
-    burn_rate = "$BURN_RATE" : nat;
-    interval_between_timers_in_seconds = "$INTERVAL" : nat64;
+    burn_rate = $BURN_RATE : nat;
+    interval_between_timers_in_seconds = $INTERVAL : nat64;
   },
 )"
 
-if [ $CONFIG != $EXPECTED_CONFIG ]; then
-    echo "ERROR in get_config"
+if [ "$CONFIG" != "$EXPECTED_CONFIG" ]; then
+    echo "ERROR in get_config."
     EXIT SIGINT
 fi
 
-if [ $(get_balance) != "100_000_000_000" ]; then
-    EXIT SIGINT
-fi
-sleep $INTERVAL
-
-if [ $(get_balance) != "90_000_000_000" ]; then
+if [ "$(get_balance)" != "100_000_000_000" ]; then
     EXIT SIGINT
 fi
 sleep $INTERVAL
 
-if [ $(get_balance) != "80_000_000_000" ]; then
+if [ "$(get_balance)" != "90_000_000_000" ]; then
+    EXIT SIGINT
+fi
+sleep $INTERVAL
+
+if [ "$(get_balance)" != "80_000_000_000" ]; then
     EXIT SIGINT
 fi
 
