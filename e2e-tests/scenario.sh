016f830b61b5a6d4dc5104af026e85e4e0a715ef
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-25 07:55:19 +0000

    .

--- e2e-tests/scenario.sh
+++ e2e-tests/scenario.sh
@@ -6,11 +6,12 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PATH="$SCRIPT_DIR/target/bin:$PATH"
 
 set_correct_replica_and_canister_sandbox(){
-    chmod +x replica canister_sandbox
+    chmod +x replica canister_sandbox ic-https-outcalls-adapter
     PATH_TO_DFX_CACHE="$HOME/.cache/dfinity/versions/$(dfx --version | awk '{ print $2 }')"
-    rm -f "$PATH_TO_DFX_CACHE/replica" "$PATH_TO_DFX_CACHE/canister_sandbox"
+    rm -f "$PATH_TO_DFX_CACHE/replica" "$PATH_TO_DFX_CACHE/canister_sandbox" "$PATH_TO_DFX_CACHE/ic-https-outcalls-adapter" 
     cp canister_sandbox "$PATH_TO_DFX_CACHE"
     cp replica "$PATH_TO_DFX_CACHE"
+    cp ic-https-outcalls-adapter "$PATH_TO_DFX_CACHE"
 }
 
 get_balance() {
