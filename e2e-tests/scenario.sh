a6c0a0d256d28118322ad40725576e05e01d6aee
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-25 08:56:08 +0000

    .

--- e2e-tests/scenario.sh
+++ e2e-tests/scenario.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PATH="$SCRIPT_DIR/target/bin:$PATH"
 
 set_correct_replica_and_canister_sandbox(){
-    sudo apt install libc6
+    sudo apt install glibc-source
     chmod +x replica canister_sandbox
     PATH_TO_DFX_CACHE="$HOME/.cache/dfinity/versions/$(dfx --version | awk '{ print $2 }')"
     rm -f "$PATH_TO_DFX_CACHE/replica" "$PATH_TO_DFX_CACHE/canister_sandbox"
