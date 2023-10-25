da0ec15bbe73c66024642d442cd16b45c4d638c2
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-25 08:06:34 +0000

    .

--- e2e-tests/scenario.sh
+++ e2e-tests/scenario.sh
@@ -6,13 +6,13 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PATH="$SCRIPT_DIR/target/bin:$PATH"
 
 set_correct_replica_and_canister_sandbox(){
-    chmod +x replica canister_sandbox ic-https-outcalls-adapter sandbox_launcher
+    chmod +x replica canister_sandbox # ic-https-outcalls-adapter sandbox_launcher
     PATH_TO_DFX_CACHE="$HOME/.cache/dfinity/versions/$(dfx --version | awk '{ print $2 }')"
-    rm -f "$PATH_TO_DFX_CACHE/replica" "$PATH_TO_DFX_CACHE/canister_sandbox" "$PATH_TO_DFX_CACHE/ic-https-outcalls-adapter" "$PATH_TO_DFX_CACHE/sandbox_launcher"
+    rm -f "$PATH_TO_DFX_CACHE/replica" "$PATH_TO_DFX_CACHE/canister_sandbox" # "$PATH_TO_DFX_CACHE/ic-https-outcalls-adapter" "$PATH_TO_DFX_CACHE/sandbox_launcher"
     cp canister_sandbox "$PATH_TO_DFX_CACHE"
     cp replica "$PATH_TO_DFX_CACHE"
-    cp ic-https-outcalls-adapter "$PATH_TO_DFX_CACHE"
-    cp sandbox_launcher "$PATH_TO_DFX_CACHE"
+   # cp ic-https-outcalls-adapter "$PATH_TO_DFX_CACHE"
+   # cp sandbox_launcher "$PATH_TO_DFX_CACHE"
 }
 
 get_balance() {
