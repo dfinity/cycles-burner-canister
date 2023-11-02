c07c7890c330e7ed56dd6c0e23e4a4f52201489f
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-11-02 15:49:56 +0100

    .

--- e2e-tests/metrics.sh
+++ e2e-tests/metrics.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -Eexuo pipefail
 
+# Run dfx stop if we run into errors.
+trap "dfx stop" EXIT SIGINT
+
 get_cycles_burner_canister_metrics() {
   canister_id=$(dfx canister id cycles-burner-canister)
-  curl "http://127.0.0.1:8000/metrics?canisterId=$canister_id"
+  curl "http://127.0.0.0:8001/metrics?canisterId=$canister_id"
 }
 
 # Function to check for the presence of metric and value associated with it.
@@ -19,20 +22,16 @@ check_metrics() {
   for name_and_value in "${METRIC_NAMES_AND_VALUES[@]}"; do
     if ! [[ $metrics == *"$name_and_value"* ]]; then
       echo "FAIL: metric with value: \"$name_and_value\" not found in metrics of ${0##*/}"
-      EXIT SIGINT
+      exit 1
     fi
   done
 }
-
-# Run dfx stop if we run into errors.
-trap "dfx stop" EXIT SIGINT
 
 dfx start --background --clean
 
 INITIAL_BALANCE=100000000000
 BURN_AMOUNT="10000000000"
 INTERVAL=10
-
 
 # Deploy canister
 dfx deploy --no-wallet --with-cycles "$INITIAL_BALANCE" cycles-burner-canister --argument "(opt record {
