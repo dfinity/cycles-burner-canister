96d164fc2195882b79de11bdeaf443e9c63678f6
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-11-02 15:58:43 +0100

    cleanup all

--- e2e-tests/metrics.sh
+++ e2e-tests/metrics.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 set -Eexuo pipefail
 
-get_cycles_burner_canister_metrics() {
-  canister_id=$(dfx canister id cycles-burner-canister)
-  curl "http://127.0.0.0:8001/metrics?canisterId=$canister_id"
-}
+# get_cycles_burner_canister_metrics() {
+#   canister_id=$(dfx canister id cycles-burner-canister)
+#   curl "http://127.0.0.0:8001/metrics?canisterId=$canister_id"
+# }
 
-# Function to check for the presence of metric and value associated with it.
-check_metrics() {
-  METRIC_NAMES_AND_VALUES=(
-    "burn_amount $1"
-    "interval_between_timers_in_seconds $2"
-    "counter $3"
-    "total_cycles_burnt $4"
-  )
+# # Function to check for the presence of metric and value associated with it.
+# check_metrics() {
+#   METRIC_NAMES_AND_VALUES=(
+#     "burn_amount $1"
+#     "interval_between_timers_in_seconds $2"
+#     "counter $3"
+#     "total_cycles_burnt $4"
+#   )
 
-  metrics=$(get_cycles_burner_canister_metrics)
-  for name_and_value in "${METRIC_NAMES_AND_VALUES[@]}"; do
-    if ! [[ $metrics == *"$name_and_value"* ]]; then
-      echo "FAIL: metric with value: \"$name_and_value\" not found in metrics of ${0##*/}"
-      exit 1
-    fi
-  done
-}
+#   metrics=$(get_cycles_burner_canister_metrics)
+#   for name_and_value in "${METRIC_NAMES_AND_VALUES[@]}"; do
+#     if ! [[ $metrics == *"$name_and_value"* ]]; then
+#       echo "FAIL: metric with value: \"$name_and_value\" not found in metrics of ${0##*/}"
+#       exit 1
+#     fi
+#   done
+# }
 
 # Run dfx stop if we run into errors.
-trap "dfx stop" EXIT SIGINT
+# trap "dfx stop" EXIT SIGINT
 
-dfx start --background --clean
+# dfx start --background --clean
 
-INITIAL_BALANCE=100000000000
-BURN_AMOUNT="10000000000"
-INTERVAL=10
+# INITIAL_BALANCE=100000000000
+# BURN_AMOUNT="10000000000"
+# INTERVAL=10
 
 # # Deploy canister
 # dfx deploy --no-wallet --with-cycles "$INITIAL_BALANCE" cycles-burner-canister --argument "(opt record {
@@ -49,6 +49,6 @@ INTERVAL=10
 
 # check_metrics $BURN_AMOUNT $INTERVAL 2 $((2 * BURN_AMOUNT))
 
-echo "SUCCESS"
+# echo "SUCCESS"
 
-exit 0
+# exit 0
